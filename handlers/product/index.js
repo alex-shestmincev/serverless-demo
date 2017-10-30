4d32112d2987b81032bbdfd1dde60f0185c4ed6d
Alexander Shestmintsev <alexander.shestmintsev@dev-pro.net>
2017-10-31 00:04:19 +0200

    Added routes

--- handlers/product/index.js
+++ handlers/product/index.js
@@ -1,8 +1,51 @@
 const serverless = require('serverless-http');
 const app = require('express')();
+const bodyParser  = require('body-parser');
 
-app.get('', function (req, res) {
-  res.send('Hello World')
+app.use(bodyParser.json());
+
+app.get('/product', function (req, res) {
+
+  const result = {
+    params: req.params,
+    body: req.body,
+    query: req.query,
+    headers: req.headers,
+  }
+  res.json(result)
+});
+
+app.get('/product/:id', function (req, res) {
+
+  const result = {
+    params: req.params,
+    body: req.body,
+    query: req.query,
+    headers: req.headers,
+  }
+  res.json(result)
+});
+
+app.get('/product/:id/:name', function (req, res) {
+
+  const result = {
+    params: req.params,
+    body: req.body,
+    query: req.query,
+    headers: req.headers,
+  }
+  res.json(result)
+});
+
+app.post('/product', function (req, res) {
+console.log('req', req)
+  const result = {
+    params: req.params,
+    body: req.body,
+    query: req.query,
+    headers: req.headers,
+  }
+  res.json(result)
 });
 
 module.exports.handler = serverless(app);
